476f5572bbea9f602d84d893812f83f6d19b8015
Nuno Costa <up201906272@fe.up.pt>
2021-03-04 12:42:19 +0000

    Add ex13

---
+++ tp_3-4/ex13/ex13a/ex13a.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define MAXLINE 20
+
+int main(){
+    int   n, fd[2];
+    pid_t pid;
+    char  line[MAXLINE];
+    if (pipe(fd) < 0) {
+        fprintf(stderr, "pipe error\n");
+        exit(1);
+    } 
+    
+    if ( (pid = fork()) < 0) {
+        fprintf(stderr, "fork error\n");
+        exit(2);
+    } else if (pid > 0) {
+        /* parent */
+        close(fd[0]); 
+        /* closes receptor side of the pipe  */
+        write(fd[1], "Operating", 9);
+    } else {                      
+        /* filho */
+        close(fd[1]);           
+        /* closes emmiter side of the pipe */
+        n = read(fd[0], line, MAXLINE);
+        char* st = malloc(sizeof(char) * n);
+        memcpy(st, line, n);
+        printf("%s Systems", st);
+        free(st);
+    }
+    exit(0);
+}
---
+++ tp_3-4/ex13/ex13b/ex13b.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+#define MAXLINE 20
+
+int main(){
+    int   n, fd[2];
+    pid_t pid;
+    char  line[MAXLINE];
+    
+    if (mkfifo("np",0666) < 0){
+        fprintf(stderr, "pipe error\n");
+        exit(1);
+    } 
+    
+    if ( (pid = fork()) < 0) {
+        fprintf(stderr, "fork error\n");
+        exit(2);
+    } 
+
+    if (pid > 0) {
+        /* parent */
+
+        int np;
+        while ((np = open ("np", O_WRONLY)) < 0);          
+
+        write(np, "Operating", 9);
+        close(np);
+    } else {                      
+        /* filho */
+        int np;
+        while ((np = open ("np", O_RDONLY)) < 0);          
+
+        n = read(np, line, MAXLINE);
+        char* st = malloc(sizeof(char) * n);
+        memcpy(st, line, n);
+        printf("%s Systems", st);
+        free(st);
+        close(np);
+    }
+    exit(0);
+}
